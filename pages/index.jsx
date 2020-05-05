b62112771a537f948d7efa1f6334e6a80309dac6
Your marcdwest32 <marcdwest@gmail.com>
2020-05-04 22:02:18 -0500

    convert to functional

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,107 +1,90 @@
+import React, { useEffect, useContext, useState } from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 
-class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isArticleVisible: false,
-      timeout: false,
-      articleTimeout: false,
-      article: '',
-      loading: 'is-loading',
-    };
-    this.handleOpenArticle = this.handleOpenArticle.bind(this);
-    this.handleCloseArticle = this.handleCloseArticle.bind(this);
-  }
+const IndexPage = ({ timeoutId }) => {
+  const [loading, setLoading] = useState('is-loading');
+  const [timeoot, setTimeoot] = useState(false);
+  const [article, setArticle] = useState('');
+  const [articleTimeout, setArticleTimeout] = useState(false);
+  const [isArticleVisible, setIsArticleVisible] = useState(false);
 
-  componentDidMount() {
-    this.timeoutId = setTimeout(() => {
-      this.setState({ loading: '' });
+  // componentDidMount() {
+  //   this.timeoutId = setTimeout(() => {
+  //     this.setState({ loading: '' });
+  //   }, 100);
+  // }
+  useEffect(() => {
+    timeoutId = setTimeout(() => {
+      setLoading('');
     }, 100);
-  }
+  });
 
-  componentWillUnmount() {
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-  }
+  // componentWillUnmount() {
+  //   if (this.timeoutId) {
+  //     clearTimeout(this.timeoutId);
+  //   }
+  // }
 
-  handleOpenArticle(article) {
-    this.setState({
-      isArticleVisible: !this.state.isArticleVisible,
-      article,
-    });
+  const handleOpenArticle = article => {
+    setIsArticleVisible(!isArticleVisible);
+    setArticle(article);
 
     setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout,
-      });
+      setTimeoot(!timeoot);
     }, 325);
 
     setTimeout(() => {
-      this.setState({
-        articleTimeout: !this.state.articleTimeout,
-      });
+      setArticleTimeout(!articleTimeout);
     }, 350);
-  }
+  };
 
-  handleCloseArticle() {
-    this.setState({
-      articleTimeout: !this.state.articleTimeout,
-    });
+  const handleCloseArticle = () => {
+    setArticleTimeout(!articleTimeout);
 
     setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout,
-      });
+      setTimeoot(!timeoot);
     }, 325);
 
     setTimeout(() => {
-      this.setState({
-        isArticleVisible: !this.state.isArticleVisible,
-        article: '',
-      });
+      setIsArticleVisible(!isArticleVisible);
+      setArticle('');
     }, 350);
-  }
-  render() {
-    return (
-      <div
-        className={`body ${this.state.loading} ${
-          this.state.isArticleVisible ? 'is-article-visible' : ''
-        }`}
-      >
-        <div>
-          <Head>
-            <title>Next.js Starter</title>
-            <link
-              href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i'
-              rel='stylesheet'
-            />
-          </Head>
+  };
 
-          <div id='wrapper'>
-            <Header
-              onOpenArticle={this.handleOpenArticle}
-              timeout={this.state.timeout}
-            />
-            <Main
-              isArticleVisible={this.state.isArticleVisible}
-              timeout={this.state.timeout}
-              articleTimeout={this.state.articleTimeout}
-              article={this.state.article}
-              onCloseArticle={this.handleCloseArticle}
-            />
-            <Footer timeout={this.state.timeout} />
-          </div>
+  return (
+    <div
+      className={`body ${loading} ${
+        isArticleVisible ? 'is-article-visible' : ''
+      }`}
+    >
+      <div>
+        <Head>
+          <title>Next.js Starter</title>
+          <link
+            href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i'
+            rel='stylesheet'
+          />
+        </Head>
 
-          <div id='bg' />
+        <div id='wrapper'>
+          <Header onOpenArticle={handleOpenArticle} timeout={timeoot} />
+          <Main
+            isArticleVisible={isArticleVisible}
+            timeout={timeoot}
+            articleTimeout={articleTimeout}
+            article={article}
+            onCloseArticle={handleCloseArticle}
+          />
+          <Footer timeout={timeoot} />
         </div>
+
+        <div id='bg' />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default IndexPage;
