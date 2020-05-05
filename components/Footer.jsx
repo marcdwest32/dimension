9dea50363e24ec7b95d1f8a7456f79f6562be73b
Your marcdwest32 <marcdwest@gmail.com>
2020-05-05 18:03:00 -0500

    update info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Footer = ({ timeout }) => (
   <footer id='footer' style={timeout ? { display: 'none' } : {}}>
     <p className='copyright'>
-      &copy; Next.js Starter - Dimension. Design:{' '}
+    &copy; Marc D. West - {new Date().getFullYear()}. Design:{' '}
       <a href='https://html5up.net'>HTML5 UP</a>. Built with:{' '}
       <a href='https://github.com/zeit/next.js'>Next.js</a>
     </p>
