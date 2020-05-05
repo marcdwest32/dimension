6491f87747f9bbd19406703af7160b4e6b795691
Your marcdwest32 <marcdwest@gmail.com>
2020-05-04 19:11:34 -0500

    configure

---
+++ components/Header.jsx
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCat } from '@fortawesome/free-solid-svg-icons';
+
+const Header = ({ timeout, onOpenArticle }) => (
+  <header id='header' style={timeout ? { display: 'none' } : {}}>
+    <div className='logo'>
+      {/*<span className="icon fa-diamond"></span>*/}
+      <FontAwesomeIcon icon={faCat} transform='grow-36' />
+    </div>
+    <div className='content'>
+      <div className='inner'>
+        <h1>Dimension</h1>
+        <p>
+          A fully responsive site template designed by{' '}
+          <a href='https://html5up.net'>HTML5 UP</a> and released
+          <br />
+          for free under the{' '}
+          <a href='https://html5up.net/license'>Creative Commons</a> license.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            onClick={() => {
+              onOpenArticle('intro');
+            }}
+          >
+            Intro
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => {
+              onOpenArticle('work');
+            }}
+          >
+            Work
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => {
+              onOpenArticle('about');
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => {
+              onOpenArticle('contact');
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+};
+
+export default Header;
---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import '../styles/main.scss';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
