d0b17637729a541a58e26a6734a1d2eac6720f12
Your marcdwest32 <marcdwest@gmail.com>
2020-05-09 17:33:29 -0500

    change bg color and add images to Tech

--- components/articles/Tech.jsx
+++ components/articles/Tech.jsx
@@ -18,28 +18,72 @@ const Tech = ({ onCloseArticle }) => {
       </span>
       <div className='row'>
         <div className='3u 12u$(small)'>
-          <img src='/images/react.png' className='image fit'></img>
-          <h3 className='align-center'>Tech Name</h3>
+          <img src='/images/javascript.png' className='image fit'></img>
+          <h3 className='align-center'>JavaScript</h3>
         </div>
         <div className='3u 12u$(small)'>
           <img src='/images/react.png' className='image fit'></img>
-          <h3 className='align-center'>Tech Name</h3>
+          <h3 className='align-center'>React</h3>
         </div>
         <div className='3u 12u$(small)'>
-          <img src='/images/react.png' className='image fit'></img>
-          <h3 className='align-center'>Tech Name</h3>
+          <img src='/images/HTML5.png' className='image fit'></img>
+          <h3 className='align-center'>HTML5</h3>
         </div>
         <div className='3u 12u$(small)'>
-          <img src='/images/react.png' className='image fit'></img>
-          <h3 className='align-center'>Tech Name</h3>
+          <img src='/images/CSS3.png' className='image fit'></img>
+          <h3 className='align-center'>CSS3</h3>
         </div>
         <div className='3u 12u$(small)'>
-          <img src='/images/react.png' className='image fit'></img>
-          <h3 className='align-center'>Tech Name</h3>
+          <img src='/images/node.png' className='image fit'></img>
+          <h3 className='align-center'>Node</h3>
         </div>
         <div className='3u 12u$(small)'>
-          <img src='/images/react.png' className='image fit'></img>
-          <h3 className='align-center'>Tech Name</h3>
+          <img src='/images/express.png' className='image fit'></img>
+          <h3 className='align-center'>Express</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/mysql.png' className='image fit'></img>
+          <h3 className='align-center'>MySQL</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/mongodb.png' className='image fit'></img>
+          <h3 className='align-center'>MongoDB</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/next.png' className='image fit'></img>
+          <h3 className='align-center'>NextJS</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/angular.png' className='image fit'></img>
+          <h3 className='align-center'>Angular8</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/angularjs.png' className='image fit'></img>
+          <h3 className='align-center'>AngularJS</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/auth0.png' className='image fit'></img>
+          <h3 className='align-center'>Auth0</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/github.png' className='image fit'></img>
+          <h3 className='align-center'>GitHub</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/expo.png' className='image fit'></img>
+          <h3 className='align-center'>Expo</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/linux.png' className='image fit'></img>
+          <h3 className='align-center'>Linux</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/reactnative.png' className='image fit'></img>
+          <h3 className='align-center'>React Native</h3>
+        </div>
+        <div className='3u 12u$(small)'>
+          <img src='/images/vscode.png' className='image fit'></img>
+          <h3 className='align-center'>VSCode</h3>
         </div>
       </div>
       <Close onCloseArticle={onCloseArticle} />
