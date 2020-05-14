fc592c4699a5668690e79e054741a75acb0eec64
Your marcdwest32 <marcdwest@gmail.com>
2020-05-13 20:32:48 -0500

    add projects

--- components/articles/Work.jsx
+++ components/articles/Work.jsx
@@ -12,24 +12,62 @@ const Work = ({ onCloseArticle }) => {
 
   return (
     <>
-      <h2 className='major'>Work</h2>
+      <h2 className='major'>Projects</h2>
+
+      <h3>MapChat</h3>
+      <h4>Mobile Map-Based Messaging App</h4>
       <span className='image main'>
         <img src=' /images/pic02.jpg' alt='' />
       </span>
-      <p>
-        Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at
-        eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi,
-        fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec
-        lorem luctus ultrices.
-      </p>
-      <p>
-        Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero.
-        Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque
-        condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa
-        vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem
-        consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna
-        magna sed nunc rhoncus amet feugiat tempus.
-      </p>
+      <ul>
+        <li>
+          Maintained user security via OAuth 2.0 along with Google third-party
+          sign in to validate users' identity
+        </li>
+        <li>
+          Leveraged onboard location services and Google Maps API to dynamically
+          render messages for others to interact with
+        </li>
+        <li>
+          Used Expo SDK to create Android, iOS, and web app from a single
+          codebase
+        </li>
+      </ul>
+      <hr size={5} />
+      <h3>Headwaters</h3>
+      <h4>Self-Care and Wellness Management App</h4>
+      <span className='image main'>
+        <img src=' /images/pic02.jpg' alt='' />
+      </span>
+      <ul>
+        <li>Utilized Auth0 to manage third-party authentication via Google</li>
+        <li>Optimized single-page web application using React Router</li>
+        <li>
+          Architected server/client communications using ExpressJS and Axios
+        </li>
+        <li>
+          Employed AWS EC2 instance for deployment, utilizing PM2 and NGINX to
+          facilitate persistent server and optimize memory use
+        </li>
+      </ul>
+      <hr size={5} />
+      <h3>Where Should We Eat?</h3>
+      <h4>Gamified App to Help Friends Choose Local Restaurants</h4>
+      <span className='image main'>
+        <img src=' /images/pic02.jpg' alt='' />
+      </span>
+      <ul>
+        <li>
+          Created a distinct visual style using Material Design and ReactJS
+        </li>
+        <li>
+          Constructed API calls to efficiently send & retrieve necessary data
+        </li>
+        <li>
+          Used Google Cloud Deployment Manager to allocate resources and manage
+          deployment processes
+        </li>
+      </ul>
       <Close onCloseArticle={onCloseArticle} />
     </>
   );
