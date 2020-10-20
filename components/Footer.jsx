7bad1541e164f6d262af366d253b5d1a7beaa5b2
Marc West <marcdwest@gmail.com>
2020-10-20 15:00:11 -0500

    new tab target

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,18 @@ const Footer = ({ timeout }) => (
   <footer id='footer' style={timeout ? { display: 'none' } : {}}>
     <p className='copyright'>
       &copy; Marc D. West - {new Date().getFullYear()}. Design:{' '}
-      <a href='https://html5up.net'>HTML5 UP</a>. Built with:{' '}
-      <a href='https://github.com/zeit/next.js'>Next.js</a>
+      <a href='https://html5up.net' target='_blank'>
+        HTML5 UP
+      </a>
+      . Built with:{' '}
+      <a href='https://github.com/zeit/next.js' target='_blank'>
+        Next.js
+      </a>
       <br />
-      Photo Credit: <a href='https://lazyeyephoto.com/'>LazyEye Photo</a>
+      Photo Credit:{' '}
+      <a href='https://lazyeyephoto.com/' target='_blank'>
+        LazyEye Photo
+      </a>
     </p>
   </footer>
 )
